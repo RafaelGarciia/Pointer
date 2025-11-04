2e87f01264ad800996f2d4686a68b6bd0a7e7532
RafaelGarciia <rafaelgarcia.oliveira32@gmail.com>
2025-11-04 08:44:50 -0300

    Clear PopUP
    Limpando o codigo e corrigindo um bug de evendo nos pop_up

--- source/ui/pop_up.py
+++ source/ui/pop_up.py
@@ -3,7 +3,8 @@ from tkinter import ttk, messagebox
 from source.ui import utils as utl
 from source import data_base as db
 
-def new_ticker(window:tk.Tk) -> None:
+
+def new_ticker(window: tk.Tk) -> None:
 
     # Janela Top Level
     pop_up = utl.Pop_Up(window, 'New Ticket')
@@ -17,28 +18,34 @@ def new_ticker(window:tk.Tk) -> None:
     ticker_entry.pack(pady=2)
 
     # -------------- Função Save ------------- #
-    def save():
+    def save(event=None):
         ticker = ticker_entry.get().strip().upper()
         if not ticker:
             messagebox.showwarning('Aviso', 'Digite um Ativo.')
             return
-        
+
         # Formata o ticker. Ex: 'PETR4.SA'
-        ticker = (ticker if ticker.upper().endswith('.SA') else f'{ticker.upper()}.SA')
+        ticker = (
+            ticker
+            if ticker.upper().endswith('.SA')
+            else f'{ticker.upper()}.SA'
+        )
 
         # Carrega os ticker no banco de dados
         tickers = db.load_tickers()
 
         # Verifica se o ticket ja esta no banco de dados
         if ticker in tickers:
-            messagebox.showinfo('Ativo já cadastrado', f"'{ticker}' já está cadastrado.")
+            messagebox.showinfo(
+                'Ativo já cadastrado', f"'{ticker}' já está cadastrado."
+            )
             return
-        
+
         # Registra o ticker no banco de dados
         db.save_ticket(ticker)
         pop_up.destroy()
         messagebox.showinfo('Sucesso', f'{ticker} cadastrado com sucesso.')
-    
+
     # Botão de save
     ttk.Button(pop_up, text='Salvar', command=save).pack(pady=2)
 
@@ -46,10 +53,10 @@ def new_ticker(window:tk.Tk) -> None:
     ticker_entry.bind('<Return>', save)
 
 
-def edit_ticker(window:tk.Tk, item) -> None:
+def edit_ticker(window: tk.Tk, item) -> None:
     if not item:
         return
-    
+
     # Janela Top Level
     pop_up = utl.Pop_Up(window, 'Edit Ticket')
 
@@ -63,56 +70,72 @@ def edit_ticker(window:tk.Tk, item) -> None:
     ticker_entry.pack(pady=2)
 
     # -------------- Função Edit ------------- #
-    def edit():
+    def edit(event=None):
         ticker = ticker_entry.get().strip().upper()
         if not ticker:
             messagebox.showwarning('Aviso', 'Digite um Ativo.')
             return
-        
+
         # Formata o ticker. Ex: 'PETR4.SA'
-        ticker = (ticker if ticker.upper().endswith('.SA') else f'{ticker.upper()}.SA')
+        ticker = (
+            ticker
+            if ticker.upper().endswith('.SA')
+            else f'{ticker.upper()}.SA'
+        )
 
         # Carrega os ticker no banco de dados
         tickers = db.load_tickers()
 
         # Verifica se o ticket ja esta no banco de dados
         if ticker in tickers:
-            messagebox.showinfo('Ativo já cadastrado', f"'{ticker}' já está cadastrado.")
+            messagebox.showinfo(
+                'Ativo já cadastrado', f"'{ticker}' já está cadastrado."
+            )
             return
-        
+
         # Registra o ticker no banco de dados
         db.edit_ticker(f'{item[0]}.SA', ticker)
         pop_up.destroy()
         messagebox.showinfo('Sucesso', f'{ticker} cadastrado com sucesso.')
 
     # ------------- Função Delete ------------ #
-    def delete():
+    def delete(event=None):
         ticker = f'{item[0]}.SA'
         if not ticker:
             messagebox.showwarning('Aviso', 'Digite um Ativo.')
             return
-        
+
         # Formata o ticker. Ex: 'PETR4.SA'
-        ticker = (ticker if ticker.upper().endswith('.SA') else f'{ticker.upper()}.SA')
+        ticker = (
+            ticker
+            if ticker.upper().endswith('.SA')
+            else f'{ticker.upper()}.SA'
+        )
 
         # Carrega os ticker no banco de dados
         tickers = db.load_tickers()
 
         # Verifica se o ticket ja esta no banco de dados
         if not ticker in tickers:
-            messagebox.showinfo('Ativo não cadastrado', f"'{ticker}' não está cadastrado.")
+            messagebox.showinfo(
+                'Ativo não cadastrado', f"'{ticker}' não está cadastrado."
+            )
             return
-        
+
         # Registra o ticker no banco de dados
         db.remove_ticker(ticker)
         pop_up.destroy()
         messagebox.showinfo('Sucesso', f'{ticker} deletado com sucesso.')
 
     # Botão de save
-    ttk.Button(pop_up, text='Salvar', command=edit).pack(pady=5, padx=10, side='left')
+    ttk.Button(pop_up, text='Salvar', command=edit).pack(
+        pady=5, padx=10, side='left'
+    )
 
     # Botão de delete
-    ttk.Button(pop_up, text='Excluir', command=delete).pack(pady=5, padx=10, side='right')
+    ttk.Button(pop_up, text='Excluir', command=delete).pack(
+        pady=5, padx=10, side='right'
+    )
 
     # Binds
     ticker_entry.bind('<Return>', edit)
