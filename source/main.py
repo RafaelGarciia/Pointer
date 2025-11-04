cc041f5d06d96cb174099e8bcb4d9ae0f6f3481f
RafaelGarciia <rafaelgarcia.oliveira32@gmail.com>
2025-11-04 08:42:26 -0300

    Clear Main
    Limpando o codigo

--- source/main.py
+++ source/main.py
@@ -1,26 +1,28 @@
 from tkinter import Menu
 from source.ui import utils as utl
 from source.ui import frame
-
+from source import data_base
 
 
 def app():
 
+    data_base.db_init()
 
     win = utl.Window()
 
-
     top_menu = Menu(win)
-    top_menu.add_command(label='Pointer' ,command=lambda: win.show_frame(frame.Pointer_consult(win)))
-    top_menu.add_command(label='Configure', command=lambda: win.show_frame(frame.Config(win)))
 
+    config_cascade = Menu(top_menu, tearoff=False)
+    top_menu.add_cascade(label='Sistema', menu=config_cascade)
+
+    top_menu.add_command(
+        label='Pointer',
+        command=lambda: win.show_frame(frame.Pointer_consult(win)),
+    )
+    config_cascade.add_command(
+        label='Configure', command=lambda: win.show_frame(frame.Config(win))
+    )
 
     win.config(menu=top_menu)
 
-
-
     win.mainloop()
-
-
-
-
