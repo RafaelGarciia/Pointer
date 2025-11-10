007a58e5a10b59d9c38748297a6c470b665b6c71
RafaelGarciia <rafaelgarcia.oliveira32@gmail.com>
2025-11-10 15:58:16 -0300

    Data base
    Preparando o banco de dados para receber a nova função.

--- source/data_base.py
+++ source/data_base.py
@@ -3,8 +3,11 @@ import os
 
 # Caminho e nome do banco
 DB_FOLDER = 'database'
-DB_NAME = 'tickers'
+DB_NAME = 'Pointer_database'
 DB_PATH = os.path.join(DB_FOLDER, f'{DB_NAME}.db')
+
+TICKER_TABLE_NAME = 'search_ticker'
+WALLET_TABLE_NAME = 'transactions'
 
 
 def connect() -> tuple[sql.Connection, sql.Cursor]:
@@ -21,9 +24,23 @@ def connect() -> tuple[sql.Connection, sql.Cursor]:
 def db_init():
     connection, cursor = connect()
     cursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS tickers (
+        f"""
+        CREATE TABLE IF NOT EXISTS {TICKER_TABLE_NAME} (
             id TEXT UNIQUE
+        )
+    """
+    )
+    cursor.execute(
+        f"""
+        CREATE TABLE IF NOT EXISTS {WALLET_TABLE_NAME} (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            date TEXT NOT NULL,
+            type TEXT NOT NULL CHECK(type IN ('Compra','Venda')),
+            active TEXT NOT NULL,
+            qnt REAL NOT NULL,
+            price REAL NOT NULL,
+            fees REAL DEFAULT 0,
+            notes TEXT
         )
     """
     )
@@ -34,7 +51,7 @@ def db_init():
 def save_ticket(id):
     connection, cursor = connect()
     try:
-        cursor.execute('INSERT INTO tickers (id) VALUES (?)', (id,))
+        cursor.execute(f'INSERT INTO {TICKER_TABLE_NAME}(id) VALUES (?)', (id,))
         connection.commit()
     except sql.IntegrityError:
         print('IntegrityError')
@@ -43,7 +60,7 @@ def save_ticket(id):
 
 def load_tickers():
     connection, cursor = connect()
-    cursor.execute('SELECT id FROM tickers')
+    cursor.execute(f'SELECT id FROM {TICKER_TABLE_NAME}')
     data = [item[0] for item in cursor.fetchall()]
     connection.close()
     return data
@@ -53,7 +70,7 @@ def remove_ticker(id):
     """Deleta um ticker do banco de dados."""
     connection, cursor = connect()
     try:
-        cursor.execute(f'DELETE FROM tickers WHERE id = (?)', (id,))
+        cursor.execute(f'DELETE FROM {TICKER_TABLE_NAME} WHERE id = (?)', (id,))
         connection.commit()
     except Exception as ex:
         print(f'Erro {ex}')
@@ -66,7 +83,7 @@ def edit_ticker(old_id, new_id):
     connection, cursor = connect()
     try:
         cursor.execute(
-            'UPDATE tickers SET id = ? WHERE id = ?', (new_id, old_id)
+            f'UPDATE {TICKER_TABLE_NAME} SET id = ? WHERE id = ?', (new_id, old_id)
         )
         connection.commit()
     except sql.IntegrityError:
@@ -75,3 +92,22 @@ def edit_ticker(old_id, new_id):
         print(f'Erro ao editar ticker: {e}')
     finally:
         connection.close()
+
+
+def add_transaction(date_iso, inp_type, active, qnt, price, fees, notes):
+    connection, cursor = connect()
+
+    cursor.execute(f"""
+        INSERT INTO {WALLET_TABLE_NAME} (date, type, active, qnt, price, fees, notes)
+        VALUES (?, ?, ?, ?, ?, ?, ?)    
+    """, (date_iso, inp_type, active.upper().strip(), qnt, price, fees, notes))
+    connection.commit()
+    connection.close()
+
+def get_all_transactions(order_by='date DESC'):
+    connection, cursor = connect()
+
+    cursor.execute(f"SELECT id, date, type, active, qnt, price, fees, notes FROM {WALLET_TABLE_NAME} ORDER BY {order_by}")
+    rows = cursor.fetchall()
+    connection.close()
+    return rows
